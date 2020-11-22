0f5e996f51016f4d50ff893f1f6eff0d6a466619
Aniket <aniket.d2402@gmail.com>
2020-11-22 14:55:40 -0500

    Created yargs commands for add, remove, list and remove notes

---
+++ app.js
@@ -0,0 +1,64 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes');
+
+// create add notes command
+yargs.command({
+    command: 'add',
+    describe: 'Adds a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body);
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Removes a note',
+    builder:{
+        title:{
+            describe:'Note title',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title);
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'Lists all notes',
+    handler() {
+        notes.listNotes();
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'reads the command',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title);
+    }
+});
+
+yargs.parse();
